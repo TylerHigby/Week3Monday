fbad8c9b2956e9491dad8d2b4567bc088783c0e7
TylerHigby <tyhigby22@msn.com>
2023-08-21 16:48:22 -0600

    monday

--- app/services/PlayerServices.js
+++ app/services/PlayerServices.js
@@ -1,10 +1,10 @@
-// import { AppState } from "../AppState.js";
+import { AppState } from "../AppState.js";
 
 
-// class PlayerService {
-//   manageScore(playerName, updatedScore) {
-//     let player = AppState.players.find(p => p.name == playerName)
-//     player.score = updatedScore
-//     console.log(player)
-//   }
-// }
+class PlayerService {
+  manageScore(playerName, updatedScore) {
+    let player = AppState.players.find(p => p.name == playerName)
+    player.score = updatedScore
+    console.log(player)
+  }
+}
